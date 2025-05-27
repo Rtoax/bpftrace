9ff706e1ec16b681b76f7ff58c885164e2d7b8af
Rong Tao <rongtao@cestc.cn>
2025-05-27 19:02:30 +0800

    Add task_from_pid(pid) and task_release(task)
    linux v6.1 commit 3f0e6f2b41d3 ("bpf: Add bpf_task_from_pid() kfunc") introduce
    bpf_task_from_pid() kfunc to get 'struct task_struct *' from pid.
    
    For example, in killsnoop.bt, the 'struct task_struct *' pointer of the target
    process can be obtained through the pid information of the target process to
    obtain more information.
    
    Usage:
    
        tracepoint:syscalls:sys_enter_kill,
        tracepoint:syscalls:sys_enter_tkill,
        tracepoint:syscalls:sys_enter_tgkill
        {
            $ttask = task_from_pid(args.pid);
            if ($ttask) {
                /* do something */
                task_release($ttask);
            }
        }
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- src/ast/passes/codegen_llvm.cpp
+++ src/ast/passes/codegen_llvm.cpp
@@ -1762,6 +1762,34 @@ ScopedExpr CodegenLLVM::visit(Call &call)
     Value *ret = b_.CreateSkbOutput(
         scoped_skb.value(), len, data, getStructSize(hdr_t));
     return ScopedExpr(ret);
+  } else if (call.func == "task_from_pid") {
+    auto &arg = call.vargs.at(0);
+    auto scoped_arg = visit(arg);
+
+    assert(arg.type().IsIntegerTy());
+
+    if (bpftrace_.feature_->has_kernel_func(Kfunc::bpf_task_from_pid)) {
+      return ScopedExpr(CreateKernelFuncCall(Kfunc::bpf_task_from_pid,
+                                             { scoped_arg.value() },
+                                             "task_from_pid",
+                                             call));
+    } else {
+      LOG(ERROR) << "kernel not support bpf_task_from_pid()!";
+    }
+  } else if (call.func == "task_release") {
+    auto &arg = call.vargs.at(0);
+    auto scoped_arg = visit(arg);
+
+    assert(arg.type().IsPtrTy());
+
+    if (bpftrace_.feature_->has_kernel_func(Kfunc::bpf_task_release)) {
+      return ScopedExpr(CreateKernelFuncCall(Kfunc::bpf_task_release,
+                                             { scoped_arg.value() },
+                                             "task_release",
+                                             call));
+    } else {
+      LOG(ERROR) << "kernel not support bpf_task_release()!";
+    }
   } else if (call.func == "nsecs") {
     if (call.return_type.ts_mode == TimestampMode::sw_tai) {
       if (!bpftrace_.delta_taitime_.has_value())
@@ -4760,8 +4788,13 @@ llvm::Function *CodegenLLVM::DeclareKernelFunc(Kfunc kfunc, Node &call)
     }
   }
 
+  SizedType rettype = func_struct->GetField(RETVAL_FIELD_NAME).type;
+  // kfunc maybe return void
+  if (rettype.IsNoneTy())
+    rettype = CreateVoid();
+
   FunctionType *func_type = FunctionType::get(
-      b_.GetType(func_struct->GetField(RETVAL_FIELD_NAME).type, false),
+      b_.GetType(rettype, false),
       args,
       false);
 
@@ -4774,7 +4807,7 @@ llvm::Function *CodegenLLVM::DeclareKernelFunc(Kfunc kfunc, Node &call)
   fun->addFnAttr(Attribute::NoUnwind);
 
   debug_.createFunctionDebugInfo(
-      *fun, func_struct->GetField(RETVAL_FIELD_NAME).type, debug_args, true);
+      *fun, rettype, debug_args, true);
 
   return fun;
 }
