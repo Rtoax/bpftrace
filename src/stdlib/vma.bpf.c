2d5e74a1f0e85ecaf4a5bcea88fba84f5d1ec8b1
Rong Tao <rongtao@cestc.cn>
2025-10-15 10:14:29 +0800

    stdlib: uaddr: support PIE and dynamic library symbols
    First, introduce vma.bpf.c to obtain the address of the file mapped in the
    target process address space. This address can be used to correct the symbol
    addresses of dynamic libraries and PIE.
    
    Then, traverse the address space of the target process and find the minimum
    address where the ELF file is loaded by matching the ino number, so as to
    calculate the symbol offset in uaddr() and obtain the accurate symbol address.
    
    For example, C code, gcc commands and bpftrace script, and test
    procedures 1->2->3->4->5->6->7:
    
        /* hello.c */
        #include <stdio.h>
    
        static char *ps1_prompt = "[rongtao@rtoax]$ ";
        int readline(const char *ps1, const char *line) {
          return 0;
        }
    
        int main(int argc, char *argv[]) {
          readline(ps1_prompt, "echo Hello.\n");
          return 0;
        }
    
    (1) EXE: $ gcc hello.c -no-pie -o hello
    (4)      $ ./hello
    (2) PIE: $ gcc hello.c -fpie -pie -o hello-pie
    (6)      $ ./hello-pie
    
        # uaddr.bt
        uprobe:/home/rongtao/hello:readline,
        uprobe:/home/rongtao/hello-pie:readline
        {
          $addr = uaddr("ps1_prompt");
          printf("%-16s %-8d, ps1_prompt: %p %s\n", comm, pid, $addr, str(*$addr));
        }
    
    (3) $ sudo bpftrace uaddr.bt
        Attached 2 probes
    (5) hello            507158  , ps1_prompt: 0x403008 [rongtao@rtoax]$
    (7) hello-pie        507160  , ps1_prompt: 0x56429d609010 [rongtao@rtoax]$
    
    Link: https://github.com/bpftrace/bpftrace/issues/75
    Fixes: https://github.com/bpftrace/bpftrace/issues/2983
    Link: https://github.com/torvalds/linux/commit/4ac454682158
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ src/stdlib/vma.bpf.c
@@ -0,0 +1,43 @@
+#define __KERNEL__
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+
+extern int bpf_iter_task_vma_new(struct bpf_iter_task_vma *it,
+                                 struct task_struct *task, u64 addr) __ksym __weak;
+extern struct vm_area_struct *bpf_iter_task_vma_next(struct bpf_iter_task_vma *it) __ksym __weak;
+void bpf_iter_task_vma_destroy(struct bpf_iter_task_vma *it) __ksym __weak;
+
+
+unsigned long __bpf_task_map_file_min_addr(unsigned long ino)
+{
+  struct bpf_iter_task_vma vma_it;
+  struct vm_area_struct *vma;
+  struct task_struct *cur_task = bpf_get_current_task_btf();
+  unsigned long off = 0xffffffffffffffffUL;
+  _Bool found = false;
+
+  // linux >= 6.7
+  if (!bpf_iter_task_vma_new)
+    return 0;
+
+  if (bpf_iter_task_vma_new(&vma_it, cur_task, 0)) {
+    bpf_iter_task_vma_destroy(&vma_it);
+    return 0;
+  }
+
+  while ((vma = bpf_iter_task_vma_next(&vma_it))) {
+    struct file *file = vma->vm_file;
+    if (file) {
+      struct inode *inode = file->f_inode;
+      if (inode->i_ino == ino) {
+        found = true;
+        if (off > vma->vm_start) {
+          off = vma->vm_start;
+        }
+      }
+    }
+  }
+
+  bpf_iter_task_vma_destroy(&vma_it);
+  return found ? off : 0;
+}
