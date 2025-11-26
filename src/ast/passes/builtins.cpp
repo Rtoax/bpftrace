e3212eb53c500aca6cb6d76f0ed78ad5953fcf8d
Rong Tao <rongtao@cestc.cn>
2025-11-26 22:23:49 +0800

    Fix parameter parsing issues in __builtin_signal_num
    Fixed the built-in __builtin_signal_num() parameter parsing error that
    caused __builtin_signal_num() to be undefined, for example:
    
        begin {
          let $sig = "SIGINT";
          printf("%d\n", __builtin_signal_num($sig));
        }
    
        signal.bt:5:18-44: ERROR: Unknown function: '__builtin_signal_num'
          printf("%d\n", __builtin_signal_num($sig));
                         ~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    Co-authored-by: Jordan Rome <linux@jordanrome.com>

--- src/ast/passes/builtins.cpp
+++ src/ast/passes/builtins.cpp
@@ -97,7 +97,7 @@ std::optional<Expression> Builtins::visit(Call &call)
   Visitor<Builtins, std::optional<Expression>>::visit(call);
   if (call.func == "__builtin_signal_num") {
     if (call.vargs.size() != 1) {
-      call.addError() << "__builtin_signal_num expects 1 argument";
+      call.addError() << call.func << " expects 1 argument";
     } else {
       if (auto *str = call.vargs.at(0).as<String>()) {
         auto signal_num = signal_name_to_num(str->value);
@@ -105,6 +105,8 @@ std::optional<Expression> Builtins::visit(Call &call)
           call.addError() << "Invalid string for signal: " << str->value;
         }
         return ast_.make_node<Integer>(str->loc, signal_num);
+      } else {
+        call.addError() << call.func << " expects a string literal argument";
       }
     }
   } else if (call.func == "__builtin_kfunc_exist") {
