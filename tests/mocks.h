73e062425238c8866c37b440ba91bbee8dc6b306
Rong Tao <rongtao@cestc.cn>
2025-12-29 11:01:54 +0800

    Respect `missing_probes` config for bad `args` if the probe will be pruned
    There is an interesting bug: when `config::missing_probes=warn`, if a
    non-existent tracepoint is used and the built-in variable `args` is called,
    `args` will be parsed for the non-existent tracepoint, resulting in an error.
    
    For example, `tools/opensnoop.bt` on an aarch64 environment will throw an
    error because the `open(2)` system call does not exist. This is clearly
    incorrect.
    
        $ uname -m
        aarch64
    
        $ sudo bpftrace opensnoop.bt
        opensnoop.bt:49:1-35: ERROR: Tracepoint not found: syscalls:sys_enter_open
        tracepoint:syscalls:sys_enter_open,
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        opensnoop.bt:130:1-34: ERROR: Tracepoint not found: syscalls:sys_exit_open
        tracepoint:syscalls:sys_exit_open,
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Simplified analysis example (on aarch64):
    
      Works fine:
        config = { missing_probes = warn; }
        tracepoint:syscalls:sys_enter_open,
        tracepoint:syscalls:sys_enter_openat,
        tracepoint:syscalls:sys_enter_openat2
        {}
        WARNING: Unable to attach probe: tracepoint:syscalls:sys_enter_open. Skipping.
    
      Cannot work:
        config = { missing_probes = warn; }
        tracepoint:syscalls:sys_enter_open,
        tracepoint:syscalls:sys_enter_openat,
        tracepoint:syscalls:sys_enter_openat2
        {
          @filename[tid] = args.filename;
        }
        ./tp.bt:7:1-35: ERROR: Tracepoint not found: syscalls:sys_enter_open
        tracepoint:syscalls:sys_enter_open,
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    This commit passes through non-existent tracepoints and raw tracepoints in
    the case of `config::missing_probes!=error`.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/mocks.h
+++ tests/mocks.h
@@ -113,6 +113,12 @@ public:
     return true;
   }
 
+  bool is_module_loaded(const std::string &module) const override
+  {
+    return (module == "vmlinux" || module == "mock_vmlinux" ||
+            module == "kernel_mod" || module == "other_kernel_mod");
+  }
+
   Result<uint64_t> get_buffer_pages(
       bool __attribute__((unused)) /*per_cpu*/) const override
   {
