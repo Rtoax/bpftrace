0866cdfb9ec08f06fc60aaa1f7e3b7b61c8cf379
Rong Tao <rongtao@cestc.cn>
2026-02-10 11:15:33 +0800

    getopt: add description argument
    The function `getopt()` needs parameter descriptions because users don't
    want to open BT scripts to read the code; we should provide parameter
    descriptions for users. This patch adds an optional parameter to `getopt`
    as parameter description.
    
    For example: test.bt
    
      begin { getopt("a", 1, "des A"); getopt("b", 1, "des B"); }
    
      # bpftrace test.bt -- --help
      HINT: expected program options:
      --b: des B
      --a: des A
    
    Note, this patch makes getopt no longer support boolean default values,
    and instead requires an initial value to be passed in.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- src/ast/passes/named_param.cpp
+++ src/ast/passes/named_param.cpp
@@ -14,7 +14,9 @@ public:
   using Visitor<NamedParamPass>::visit;
   void visit(Expression &expr);
 
-  std::unordered_map<std::string, globalvars::GlobalVarValue> used_args;
+  std::unordered_map<std::string,
+                     std::pair<globalvars::GlobalVarValue, std::string>>
+      used_args;
   NamedParamDefaults defaults;
 
 private:
@@ -37,15 +39,30 @@ void NamedParamPass::visit(Expression &expr)
     return;
   }
 
-  if (call->vargs.size() == 2) {
-    if (!call->vargs.at(1).as<Integer>() &&
-        !call->vargs.at(1).as<NegativeInteger>() &&
-        !call->vargs.at(1).as<String>() && !call->vargs.at(1).as<Boolean>()) {
-      call->vargs.at(1).node().addError()
-          << "Second argument to 'getopt' must be a string literal, integer "
-             "literal, or a boolean literal.";
+  if (call->vargs.size() < 2 || call->vargs.size() > 3) {
+    call->addError() << "The 'getopt' function can take a maximum of "
+                     << "three arguments and a minimum of two arguments.";
+    return;
+  }
+
+  String *description = nullptr;
+  if (call->vargs.size() == 3) {
+    description = call->vargs.at(2).as<String>();
+    if (!description) {
+      call->vargs.at(2).node().addWarning()
+          << "Function 'getopt' requires a third string literal parameter"
+          << " to describe the parameters.";
       return;
     }
+  }
+
+  if (!call->vargs.at(1).as<Integer>() &&
+      !call->vargs.at(1).as<NegativeInteger>() &&
+      !call->vargs.at(1).as<String>() && !call->vargs.at(1).as<Boolean>()) {
+    call->vargs.at(1).node().addError()
+        << "Second argument to 'getopt' must be a string literal, integer "
+           "literal, or a boolean literal.";
+    return;
   }
 
   globalvars::GlobalVarValue np_default;
@@ -53,11 +70,7 @@ void NamedParamPass::visit(Expression &expr)
   auto *map_node = ast_.make_node<Map>(call->loc, arg_name->value);
   map_node->key_type = CreateInt64();
 
-  if (call->vargs.size() == 1) {
-    // boolean
-    map_node->value_type = CreateBool();
-    np_default = false;
-  } else if (auto *default_value = call->vargs.at(1).as<Boolean>()) {
+  if (auto *default_value = call->vargs.at(1).as<Boolean>()) {
     // boolean
     map_node->value_type = CreateBool();
     np_default = default_value->value;
@@ -75,34 +88,51 @@ void NamedParamPass::visit(Expression &expr)
     np_default = default_value->value;
   }
 
-  if (used_args.contains(arg_name->value) &&
-      used_args.at(arg_name->value) != np_default) {
-    std::string pre_value;
-    if (std::holds_alternative<std::string>(used_args.at(arg_name->value))) {
-      pre_value = std::get<std::string>(used_args.at(arg_name->value));
-    } else if (std::holds_alternative<int64_t>(used_args.at(arg_name->value))) {
-      pre_value = std::to_string(
-          std::get<int64_t>(used_args.at(arg_name->value)));
-    } else if (std::holds_alternative<uint64_t>(
-                   used_args.at(arg_name->value))) {
-      pre_value = std::to_string(
-          std::get<uint64_t>(used_args.at(arg_name->value)));
-    } else {
-      pre_value = std::get<bool>(used_args.at(arg_name->value)) ? "true"
-                                                                : "false";
+  if (used_args.contains(arg_name->value)) {
+    if (used_args.at(arg_name->value).first != np_default) {
+      std::string pre_value;
+      if (std::holds_alternative<std::string>(
+              used_args.at(arg_name->value).first)) {
+        pre_value = std::get<std::string>(used_args.at(arg_name->value).first);
+      } else if (std::holds_alternative<int64_t>(
+                     used_args.at(arg_name->value).first)) {
+        pre_value = std::to_string(
+            std::get<int64_t>(used_args.at(arg_name->value).first));
+      } else if (std::holds_alternative<uint64_t>(
+                     used_args.at(arg_name->value).first)) {
+        pre_value = std::to_string(
+            std::get<uint64_t>(used_args.at(arg_name->value).first));
+      } else {
+        pre_value = std::get<bool>(used_args.at(arg_name->value).first)
+                        ? "true"
+                        : "false";
+      }
+      call->addError()
+          << "Command line option '" << arg_name->value
+          << "' needs to have the same default value in all places "
+             "it is used. Previous default value: "
+          << pre_value;
+      return;
     }
-    call->addError() << "Command line option '" << arg_name->value
-                     << "' needs to have the same default value in all places "
-                        "it is used. Previous default value: "
-                     << pre_value;
-    return;
+
+    if (description &&
+        used_args.at(arg_name->value).second != description->value) {
+      call->addError() << "Command line option '" << arg_name->value
+                       << "' needs to have the same description in all places "
+                          "it is used. Previous description: "
+                       << used_args.at(arg_name->value).second;
+      return;
+    }
   }
 
   auto *index = ast_.make_node<Integer>(map_node->loc, 0);
   expr.value = ast_.make_node<MapAccess>(map_node->loc, map_node, index);
 
-  used_args[arg_name->value] = np_default;
-  defaults.defaults[arg_name->value] = std::move(np_default);
+  used_args[arg_name->value] = std::make_pair(np_default,
+                                              description ? description->value
+                                                          : "");
+  defaults.defaults[arg_name->value] = std::make_pair(
+      std::move(np_default), description ? description->value : "");
 }
 
 Pass CreateNamedParamsPass()
