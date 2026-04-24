Rtoax/bpftrace

commit 401c6394bd874744475a09f7f287ca9ae76192cf

tools/{execsnoop,naptime}: Apply stdlib ppid/pcomm (#4744)
* stdlib: base.bt: add pcomm() to get parent process command

Some bpftrace/tools access task->real_parent->{pid,comm}, add pcomm() to
replace all of them with stdlib macros.

This command add variant ppid(), and add macros pcomm()/pcomm(task).

Signed-off-by: Rong Tao <rongtao@cestc.cn>

* tools/{execsnoop,naptime}: Apply stdlib ppid/pcomm

tools/{execsnoop,naptime} better use stdlib ppid/pcomm macros instead of
structure field access.

Signed-off-by: Rong Tao <rongtao@cestc.cn>

---------

Signed-off-by: Rong Tao <rongtao@cestc.cn>

src/stdlib/base.bt
@@ -674,10 +674,26 @@ macro override(expr) {
 // * `pid(curr_ns)` - The process ID as seen from the PID namespace of bpftrace.
 // * `pid(init)` - The process ID as seen from the initial PID namespace.
 
+// :variant uint32 ppid()
 // :variant uint32 ppid(struct task_struct * task)
-// Get the pid of the parent process
+// Get the pid of the parent process for the passed task or the current task if called without arguments.
 macro ppid(task) {
     task->real_parent->pid
+}
+
+macro ppid() {
+    ppid(curtask)
+}
+
+// :variant string pcomm()
+// :variant string pcomm(struct task_struct * task)
+// Get the name of the parent process for the passed task or the current task if called without arguments.
+macro pcomm(task) {
+    task->real_parent->comm
+}
+
+macro pcomm() {
+    pcomm(curtask)
 }
 
 // :function print

tools/execsnoop.bt
@@ -57,7 +57,6 @@ BEGIN
 
 tracepoint:syscalls:sys_enter_exec*
 {
-	$task = (struct task_struct *)curtask;
-	printf("%15s %-7d %-7d ", strftime("%H:%M:%S.%f", nsecs), pid, $task->real_parent->pid);
+	printf("%15s %-7d %-7d ", strftime("%H:%M:%S.%f", nsecs), pid, ppid);
 	join(args.argv);
 }

tools/naptime.bt
@@ -43,9 +43,7 @@ BEGIN
 tracepoint:syscalls:sys_enter_nanosleep
 /args.rqtp->tv_sec + args.rqtp->tv_nsec/
 {
-	$task = (struct task_struct *)curtask;
 	time("%H:%M:%S ");
-	printf("%-6d %-16s %-6d %-16s %d.%03d\n", $task->real_parent->pid,
-	    $task->real_parent->comm, pid, comm,
-	    args.rqtp->tv_sec, (uint64)args.rqtp->tv_nsec / 1e6);
+	printf("%-6d %-16s %-6d %-16s %d.%03d\n", ppid, pcomm, pid, comm,
+		args.rqtp->tv_sec, (uint64)args.rqtp->tv_nsec / 1e6);
 }

commit ab0e815f4103a342df1904f45e3c49782d9fb980

stdlib: {pcomm,ppid}: Distinguishing between thread and process
You cannot use either `group_leader` or `real_parent` to get the parent
process's name and PID because they are different for threads and processes.

If a task_struct is the leader of a thread group, his `group_leader` is
himself, which is obviously wrong. then its pid is equal to its tid. We use
this to determine whether to use `group_leader` or `real_parent`.

At the same time, I added two test cases.

Link [1] contains a parent-child relationship diagram for processes and threads.

[1] https://github.com/bpftrace/bpftrace/pull/5132

Fixes: 401c6394bd87 ("tools/{execsnoop,naptime}: Apply stdlib ppid/pcomm (#4744)")
Fixes: c9e6d644104c ("Use group_leader for pcomm (#4795)")
Link: https://github.com/bpftrace/bpftrace/issues/782
Signed-off-by: Rong Tao <rongtao@cestc.cn>

src/stdlib/base.bt
@@ -910,7 +910,11 @@ macro write_user(dst, src, len)
 // Get the pid of the parent process for the passed task or the current task if called without arguments.
 macro ppid(task)
 {
-  task.real_parent.pid
+  if (pid == tid) {
+    task.real_parent.pid
+  } else {
+    task.group_leader.pid
+  }
 }
 
 macro ppid()
@@ -920,10 +924,14 @@ macro ppid()
 
 // :variant string pcomm()
 // :variant string pcomm(struct task_struct * task)
-// Get the name of the process for the passed task or the current task if called without arguments. This is an alias for (task->group_leader->comm).
+// Get the name of the parent process for the passed task or the current task if called without arguments.
 macro pcomm(task)
 {
-  task.group_leader.comm
+  if (pid == tid) {
+    task.real_parent.comm
+  } else {
+    task.group_leader.comm
+  }
 }
 
 macro pcomm()

tests/runtime/stdlib
@@ -21,6 +21,18 @@ NAME comm from pid
 PROG iter:task { @ = comm(pid); exit() }
 EXPECT @: bpftrace
 
+NAME pcomm for thread
+RUN {{BPFTRACE}} -e 'uprobe:./testprogs/multi_threads:stub { printf("%s %s\n", comm, pcomm); }'
+EXPECT son-thread parent-thread
+EXPECT grandson-thread parent-thread
+AFTER ./testprogs/multi_threads
+
+NAME pcomm for process
+RUN {{BPFTRACE}} -e 'uprobe:./testprogs/multi_procs:stub { printf("%s %s\n", comm, pcomm); }'
+EXPECT son-proc parent-proc
+EXPECT grandson-proc son-proc
+AFTER ./testprogs/multi_procs
+
 NAME delete map
 PROG begin { @ = 1; @a[1] = 1; @b[1, 2] = 2; if (delete(@)) { printf("ok1\n"); } delete(@a, 1); if (delete(@b, (1, 2))) { printf("ok2\n"); } if (!delete(@b, (5, 6))) { printf("ok3\n"); } }
 EXPECT ok1

tests/testprogs/multi_procs.c
@@ -1,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/prctl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/* could be probed */
+void stub(void)
+{
+}
+
+void setname(const char *name)
+{
+  if (prctl(PR_SET_NAME, name, 0, 0, 0)) {
+    perror("prctl: failed to set process name");
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  pid_t son;
+
+  setname("parent-proc");
+
+  son = fork();
+  if (son == 0) {
+    pid_t grandson;
+
+    setname("son-proc");
+    stub();
+    grandson = fork();
+    if (grandson == 0) {
+      setname("grandson-proc");
+      stub();
+      exit(0);
+    }
+    waitpid(grandson, NULL, 0);
+    exit(0);
+  }
+
+  waitpid(son, NULL, 0);
+  return 0;
+}

tests/testprogs/multi_threads.c
@@ -1,0 +1,62 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/* could be probed in tests */
+void stub(void)
+{
+}
+
+void setname(const char *name)
+{
+  if (pthread_setname_np(pthread_self(), name)) {
+    perror("pthread_setname_np");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void *grandson_work(void *)
+{
+  setname("grandson-thread");
+  stub();
+  return NULL;
+}
+
+void *son_work(void *)
+{
+  pthread_t tid;
+
+  setname("son-thread");
+  stub();
+
+  if (pthread_create(&tid, NULL, grandson_work, 0)) {
+    perror("pthread_create");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pthread_join(tid, NULL)) {
+    perror("pthread_join");
+    exit(EXIT_FAILURE);
+  }
+
+  return NULL;
+}
+
+int main(void)
+{
+  pthread_t tid;
+
+  setname("parent-thread");
+
+  if (pthread_create(&tid, NULL, son_work, 0)) {
+    perror("pthread_create");
+    exit(EXIT_FAILURE);
+  }
+
+  if (pthread_join(tid, NULL)) {
+    perror("pthread_join");
+    exit(EXIT_FAILURE);
+  }
+  return 0;
+}