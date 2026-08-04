a11f0df76ad28813525ee947bbf58e7e83fb1376
Rong Tao <rongtao@cestc.cn>
2026-08-04 09:02:10 +0800

    Allocate a padding length string for Config::max_strlen
    Since [1] defaults to adding 1 ('\0') to the length of strings less
    than 1024, this causes the bpf verifier to complain.
    
    [1] commit 3049bbf4170f ("printf: use `OpaqueValue` for formatting")
    
    Add an interface Config::pad_max_strlen() to get the length of
    max_strlen+1 aligned upwards by 8 bytes. And remove 'pad' parameter
    from IRBuilderBPF::CreateGetStrAllocation() argument list.
    
    codegen 'str(args.filename)' pseudocode:
    
        buf = CreateGetStrAllocation() {
                createAllocation() {
                  createScratchBuffer() {
                    get_sized_type() {
                      /* actually allocate max_strlen */
                    }
                  }
                }
              };
        CreateMemsetBPF(buf, ..., max_strlen + padding); // invalid access
    
    Reproduce the bpf verifier error example:
    
        config = { max_strlen = 64; }
        tracepoint:syscalls:sys_enter_openat
        {
          str(args.filename);
        }
    
    On centos stream 10, 6.12.0-233.el10.x86_64, llvm 22.1.1:
    
        ERROR: Error loading BPF program for tracepoint_syscalls_sys_enter_openat_1. Error code: -13 (Permission denied).
        Kernel error log:
        0: R1=ctx() R10=fp0
        ;  @ bpftrace.bpf.o:0
        0: (bf) r6 = r1                       ; R1=ctx() R6=ctx()
        1: (18) r1 = 0xffffce058085a000       ; R1=map_value(map=26a28e50.rodata,ks=4,vs=8)
        3: (79) r7 = *(u64 *)(r1 +0)          ; R1=map_value(map=26a28e50.rodata,ks=4,vs=8) R7=7
        4: (85) call bpf_get_smp_processor_id#8       ; R0=scalar(smin=smin32=0,smax=umax=smax32=umax32=7,var_off=(0x0; 0x7))
        5: (5f) r7 &= r0                      ; R0=scalar(smin=smin32=0,smax=umax=smax32=umax32=7,var_off=(0x0; 0x7)) R7=scalar(smin=smin32=0,smax=umax=smax32=umax32=7,var_off=(0x0; 0x7))
        6: (67) r7 <<= 6                      ; R7=scalar(smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0))
        7: (18) r1 = 0xffffce058088a000       ; R1=map_value(map=.data.get_str_b,ks=4,vs=512)
        9: (0f) r1 += r7                      ; R1=map_value(map=.data.get_str_b,ks=4,vs=512,smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0)) R7=scalar(smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0))
        10: (b7) r2 = -1                      ; R2=-1
        11: (7b) *(u64 *)(r1 +0) = r2         ; R1=map_value(map=.data.get_str_b,ks=4,vs=512,smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0)) R2=-1
        12: (7b) *(u64 *)(r1 +8) = r2         ; R1=map_value(map=.data.get_str_b,ks=4,vs=512,smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0)) R2=-1
        13: (7b) *(u64 *)(r1 +16) = r2        ; R1=map_value(map=.data.get_str_b,ks=4,vs=512,smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0)) R2=-1
        14: (7b) *(u64 *)(r1 +24) = r2        ; R1=map_value(map=.data.get_str_b,ks=4,vs=512,smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0)) R2=-1
        15: (7b) *(u64 *)(r1 +32) = r2        ; R1=map_value(map=.data.get_str_b,ks=4,vs=512,smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0)) R2=-1
        16: (7b) *(u64 *)(r1 +40) = r2        ; R1=map_value(map=.data.get_str_b,ks=4,vs=512,smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0)) R2=-1
        17: (7b) *(u64 *)(r1 +48) = r2        ; R1=map_value(map=.data.get_str_b,ks=4,vs=512,smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0)) R2=-1
        18: (7b) *(u64 *)(r1 +56) = r2        ; R1=map_value(map=.data.get_str_b,ks=4,vs=512,smin=smin32=0,smax=umax=smax32=umax32=448,var_off=(0x0; 0x1c0)) R2=-1
        19: (73) *(u8 *)(r1 +64) = r2
        invalid access to map value, value_size=512 off=512 size=1
        R1 max value is outside of the allowed memory range
        processed 18 insns (limit 1000000) max_states_per_insn 0 total_states 0 peak_states 0 mark_read 0
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- src/ast/irbuilderbpf.h
+++ src/ast/irbuilderbpf.h
@@ -133,8 +133,7 @@ public:
   CallInst *CreateThisCpuPtr(Value *var, const Location &loc);
   CallInst *CreateGetSocketCookie(Value *var, const Location &loc);
   Value *CreateGetStrAllocation(const std::string &name,
-                                const Location &loc,
-                                uint64_t pad = 0);
+                                const Location &loc);
   Value *CreateGetFmtStringArgsAllocation(StructType *struct_type,
                                           const std::string &name,
                                           const Location &loc);
